ee0ab8aab6c42c27c030edaed45a8c4d7d9930a1
bestfedfe <bestfedfe@gmail.com>
2020-02-14 17:04:42 +0700

    test

--- registration.php
+++ registration.php
@@ -170,8 +170,8 @@ h3,h1 {
 		$email = mysqli_real_escape_string($con, $email);
 		$password = stripslashes($_REQUEST['password']);
 		$password = mysqli_real_escape_string($con, $password);
-		$trn_date = date("Y-m-d H:i:s");
-
+		$trn_date = date_default_timezone_set('Asia/Bangkok');
+//  date("Y-m-d H:i:s");
 		$query = "INSERT INTO users (username , password, email, trn_date)
 					VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
 
